8ed839cae846407a2e11235ab51e789fe1835118
Dakota M <dakota.paige@hotmail.com>
2018-02-09 08:46:36 -0800

    Added SVGs, classes & basic javascript

---
+++ src/partials/Paddle.js
@@ -0,0 +1,48 @@
+import { SVG_NS } from '../settings';
+
+export default class Paddle {
+  constructor(boardHeight, width, height, x, y, up, down) {
+    this.boardHeight = boardHeight;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speed = 10;
+    this.score = 0;
+
+    document.addEventListener('keydown', event => {
+      switch (event.key) {
+        case up:
+          this.up();
+          break;
+        case down:
+          this.down();
+          break;
+      }
+    });
+  }
+  
+  //getting the maxiumum number between 0 (top of the board) and the current position of the paddle
+  up() {
+    this.y = Math.max(this.y - this.speed, 0);
+  }
+
+  //getting the minimum number between the height of the board minus the height of the paddle
+  //(so paddle doesn't extend past board), or the speed plus height. (move down)
+  down() {
+    this.y = Math.min((this.y + this.speed), (this.boardHeight-this.height));
+
+  }
+
+  //rendering SVG images
+  render(svg) {
+    let rect = document.createElementNS(SVG_NS, 'rect');
+    rect.setAttributeNS(null, 'fill', 'mediumpurple'); 
+    rect.setAttributeNS(null, 'width', this.width);
+    rect.setAttributeNS(null, 'height', this.height);
+    rect.setAttributeNS(null, 'x', this.x);
+    rect.setAttributeNS(null, 'y', this.y);
+
+    svg.appendChild(rect);
+  }
+}
